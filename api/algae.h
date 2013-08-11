3e6a25416d647a40618322709c3f158a313be47c
Anthony de Almeida Lopes <anthony.de.almeida.lopes@falsifiable.net>
2013-08-11 16:20:00 +0200

    updating docs

---
+++ api/algae.h
@@ -0,0 +1,22 @@
+/*
+    Copyright © 2013 Anthony de Almeida Lopes
+ */
+#ifndef ALGAE_H
+#define ALGAE_H
+
+/* Inspired by libCello */
+
+#define function void const *
+#define parameter void const * const
+#define structure struct
+#define enumeration enum
+
+/* Function modifiers */
+#define local static
+#define global extern
+
+#define let void *
+
+#define alias(x, y) typedef x y
+
+#endif /* ALGAE_H */
